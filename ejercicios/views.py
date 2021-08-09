e2480c82df42e7485e11b3714e33179ae73caf0a
Gianfranco Salomone <gianfranco.s@gmail.com>
2021-08-08 22:10:46 -0300

    Changed behaviour of ejercicios

--- ejercicios/views.py
+++ ejercicios/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Ejercicio           # Importo el objeto "Ejercicio"
+import random
 
 def ejercicios(request):
-    los_ejercicios = Ejercicio.objects.all().order_by('tema')     # Trae todos los objetos de la tabla, ordenados por tema
-    a = 14;
-    b = "Un string"
+    cantEjer = len(Ejercicio.objects.all())
+    indiceAleat = random.randint(0,cantEjer-1)
+    unEjer = Ejercicio.objects.all()[indiceAleat]
+    
     contexto = {
-        'los_ejercicios':los_ejercicios,
-    }             
+        'ejercicio':unEjer
+    }
+    
     return render(request,'ejercicios/ejercicios.html',contexto)
-
