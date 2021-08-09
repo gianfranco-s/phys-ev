e37fa8a96a43a3591ad518afb66d551488654938
Gianfranco Salomone <gianfranco.s@gmail.com>
2021-08-08 21:41:27 -0300

    Changed behaviour of start page

--- physicsEval/views.py
+++ physicsEval/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render,redirect
 
 def inicio(request):  # El usuario llegó directamente desde el www
     # Redireccionar al signup, que estará dentro de physicsEval/usuarios
-    return redirect('accounts/signup')
+    # return redirect('accounts/signup')
+
+    return render(request,'inicio.html')
     
 
 def about(request):  # El usuario vino desde urls, con /about
