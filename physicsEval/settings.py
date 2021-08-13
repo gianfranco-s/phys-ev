553068fe8a66a9cead01a5c26102aa79fdf0683f
Gianfranco Salomone <gianfranco.s@gmail.com>
2021-08-13 20:56:44 -0300

    HTTP methods are working

--- physicsEval/settings.py
+++ physicsEval/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # https://boiling-cove-03768.herokuapp.com/
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
