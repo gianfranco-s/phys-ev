e2b55b01d24f07d0af59ff80f95553c4440b8996
Gianfranco Salomone <gianfranco.s@gmail.com>
2021-08-26 02:57:24 -0300

    Database migrations

--- ejercicios/models.py
+++ ejercicios/models.py
@@ -7,6 +7,7 @@ class Ejercicio(models.Model):
     tema = models.CharField(max_length=50)     # 50 caracteres máximo
     enunciado = models.TextField()
     algoritmo = models.TextField(null=True)
+    
 
     def __str__(self):
         return self.titulo      # Representación en string
